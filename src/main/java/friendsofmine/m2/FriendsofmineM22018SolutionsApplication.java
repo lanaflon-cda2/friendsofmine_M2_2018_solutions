feebe6029a41ab119e48c3cfcc0c99cd1706be1c
raclet <raclet@irit.fr>
2018-09-26 22:56:26 +0200

    Initial commit

---
+++ src/main/java/friendsofmine/m2/FriendsofmineM22018SolutionsApplication.java
@@ -0,0 +1,12 @@
+package friendsofmine.m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsofmineM22018SolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsofmineM22018SolutionsApplication.class, args);
+    }
+}
---
+++ src/test/java/friendsofmine/m2/FriendsofmineM22018SolutionsApplicationTests.java
@@ -0,0 +1,16 @@
+package friendsofmine.m2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendsofmineM22018SolutionsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
